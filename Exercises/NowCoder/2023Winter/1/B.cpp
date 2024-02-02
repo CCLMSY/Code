ccb873b8958c7b10861d7d765867b0253060cb31
CCLMSY <2502408581@qq.com>
2024-02-02 19:10:30 +0800

    Update

---
+++ Exercises/NowCoder/2023Winter/1/B.cpp
@@ -0,0 +1,162 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*----------Consts----------*/
+const long MOD=1e9+7;
+const double eps=1e-80;
+
+const double pi = acos(-1.0);
+const long long INF=0x3fffffffffffffff;
+/*----------Consts----------*/
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+
+namespace DEFINITION
+{
+    #define scanfll(a) scanf("%lld",&a)
+    #define lowbit(x) ((x)&(-(x)))
+    #define RESET(A) memset(A,0,sizeof(A))
+    #define ALL(A) (A).begin(),(A).end()
+    #define SORT(A) sort(ALL(A))
+    #define SORT_REV(A) sort((A).rbegin(),(A).rend())
+    #define UNIQUE(A) unique(ALL(A))
+    #define Presentation(i,r) " \n"[i==r]
+    #define FORLL(i,l,r) for(ll i=l;i<=r;i++)
+    #define FORLL_rev(i,r,l) for(ll i=r;i>=l;i--)
+    #define Get_Mod(a) (((a)+MOD)%MOD)
+    #define pb push_back
+    #define NO "NO\n"
+    #define YES "YES\n"
+    #define endl '\n'
+}
+
+namespace CCLIB
+{
+    ostream& operator<<(ostream &out, const pair<ll,ll> &p) {out << '(' << p.first << ',' << p.second << ')';return out;}
+    #define create_vec(A,n) vector<ll> A(n);for(auto &x:A) cin >> x;
+
+    //扩欧返回d=gcd(a,b);x,y对应ax+by=d的解
+    ll Exgcd(ll a,ll b,ll &x,ll &y) {if(a==0&&b==0) return -1; if(b==0) {x=1;y=0;return a;} ll d=Exgcd(b,a%b,y,x); y-=a/b*x; return d;}
+
+    ll qcpow(ll x, ll b) {ll ret = 1;x=Get_Mod(x);for(; b; b >>= 1, x = 1ll * x * x % MOD) if(b & 1) ret = Get_Mod(1ll*ret*x); return ret;}
+
+    vector<ll> Fac,Fac_inv;
+    void Prepare_Factorium(ll n) {Fac.clear();Fac.resize(n+1);Fac[0]=Fac[1]=1; Fac_inv.clear();Fac_inv.resize(n+1);Fac_inv[0]=Fac_inv[1]=1; FORLL(i,2,n) {Fac[i]=Get_Mod(Fac[i-1]*i);Fac_inv[i]=CCLIB::qcpow(Fac[i],MOD-2);}}void Prepare_Combination(ll n){Prepare_Factorium(n);}
+    ll Get_Combination(ll m,ll n) {return Get_Mod(Get_Mod(Fac[m]*Fac_inv[m-n])*Fac_inv[n]);}
+
+    vector<ll> Nums;
+    void Get_Nums(string s){ Nums.clear(); ll n=s.length();ll t=0;int flag=0; FORLL(i,0,n-1) if(s[i]<='9'&&s[i]>='0'){t*=10;t+=s[i]-'0';flag++;}else if(flag){Nums.emplace_back(t);t=0;flag=0;} if(flag){Nums.emplace_back(t);t=0;flag=0;}}
+
+    template<class T>
+    void print_vec(const T &A){for(auto &x:A) cout << x << ' ';cout << endl;}
+    template<class T>
+    void print_float(T value,int digit=10){cout << fixed << setprecision(digit) << value;}
+    
+}
+
+namespace MOLDULE
+{
+    inline ll inv(ll x) {return CCLIB::qcpow(x,MOD-2);}
+    inline ll add(ll x, ll y) {return Get_Mod(x + y);}
+    inline ll addto(ll &x, ll y) {return x = add(x, y);}
+    inline ll sub(ll x, ll y) {return Get_Mod(x - y);}
+    inline ll subto(ll &x, ll y) {return x = sub(x, y);}
+    inline ll mul(ll x, ll y) {return Get_Mod(1ll*x * y);}
+    inline ll multo(ll &x, ll y) {return x = mul(x, y);}
+    inline ll div(ll x, ll y) {return Get_Mod(1ll*x*inv(y));} 
+    inline ll divto(ll &x, ll y) {return x = div(x, y);}
+}
+
+
+#define ONLINE_JUDGE
+#define FAST_IO
+#define MUTIPLE_JUDGE
+//#define CHECK_OUT_TIME
+
+using namespace DEFINITION;
+//using namespace MOLDULE;
+using namespace CCLIB;
+
+/*----------Code Area----------*/
+#define N 200005
+void solve()
+{
+    ll n;cin >> n;
+    vector<pll> v(n);
+    int ff1,ff2,ff3;ff1=ff2=ff3=0;
+    for(auto &x:v){
+        cin >> x.second >> x.first;
+        if(x.first<=0) ff1=1;
+        if(x.first>=0) ff2=1;
+        if(x.first==-1&&x.second==1) ff3++;
+        if(x.first==0&&x.second==2) ff3++;
+        if(x.first==1&&x.second==1) ff3++;
+    }
+    SORT(v);
+    int f1,f2;f1=f2=0;
+    deque<pll> dq;
+    FORLL(i,0,n-1){
+        while(dq.size()&&v[i].first-dq.front().first>1) dq.pop_front();
+        if(v[i].second==1){
+            for(auto x:dq){
+                if(x.second==2){
+                    if(v[i].first<=0) f1=1;
+                    else f2=1;
+                }
+            }
+        }else{
+            for(auto x:dq){
+                if(x.second==1){
+                    if(v[i].first<=0) f1=1;
+                    else f2=1;
+                }
+            }
+        }
+        dq.push_back(v[i]);
+    }
+    ll ans=4;
+    if(f1&&f2) ans=0;
+    else if(f1){
+        if(ff2) ans=1;
+        else ans=2;
+    }
+    else if(f2){
+        if(ff1) ans=1;
+        else ans=2;
+    }
+    else{
+        if(ff1&&ff2) ans=2;
+        else ans=3;
+    }
+    ans=min(ans,3ll-ff3);
+    cout << ans << endl;
+}
+/*----------Code Area----------*/
+
+int main(){
+    int clk = clock();
+
+#ifndef ONLINE_JUDGE
+    if(freopen("in.txt", "r", stdin)==NULL) {cout << "Fail opening in.txt!" << endl;return 0;}
+    if(freopen("out.txt", "w", stdout)==NULL) {cout << "Fail opening out.txt!" << endl;return 0;}
+#endif
+
+#ifdef FAST_IO
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+#endif
+
+#ifdef MUTIPLE_JUDGE
+    long T; cin >> T;
+    while(T--) solve();
+#else
+    solve();
+#endif
+
+#ifdef CHECK_OUT_TIME
+    cout << clock() - clk << " ms\n";
+#endif
+    return 0;
+}
---
+++ Exercises/NowCoder/2023Winter/1/E(WA).cpp
@@ -0,0 +1,157 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*----------Consts----------*/
+const long MOD=1e9+7;
+const double eps=1e-80;
+
+const double pi = acos(-1.0);
+const long long INF=0x3fffffffffffffff;
+/*----------Consts----------*/
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> pll;
+
+namespace DEFINITION
+{
+    #define scanfll(a) scanf("%lld",&a)
+    #define lowbit(x) ((x)&(-(x)))
+    #define RESET(A) memset(A,0,sizeof(A))
+    #define ALL(A) (A).begin(),(A).end()
+    #define SORT(A) sort(ALL(A))
+    #define SORT_REV(A) sort((A).rbegin(),(A).rend())
+    #define UNIQUE(A) unique(ALL(A))
+    #define Presentation(i,r) " \n"[i==r]
+    #define FORLL(i,l,r) for(ll i=l;i<=r;i++)
+    #define FORLL_rev(i,r,l) for(ll i=r;i>=l;i--)
+    #define Get_Mod(a) (((a)+MOD)%MOD)
+    #define pb push_back
+    #define NO "NO\n"
+    #define YES "YES\n"
+    #define endl '\n'
+}
+
+namespace CCLIB
+{
+    ostream& operator<<(ostream &out, const pair<ll,ll> &p) {out << '(' << p.first << ',' << p.second << ')';return out;}
+    #define create_vec(A,n) vector<ll> A(n);for(auto &x:A) cin >> x;
+
+    //扩欧返回d=gcd(a,b);x,y对应ax+by=d的解
+    ll Exgcd(ll a,ll b,ll &x,ll &y) {if(a==0&&b==0) return -1; if(b==0) {x=1;y=0;return a;} ll d=Exgcd(b,a%b,y,x); y-=a/b*x; return d;}
+
+    ll qcpow(ll x, ll b) {ll ret = 1;x=Get_Mod(x);for(; b; b >>= 1, x = 1ll * x * x % MOD) if(b & 1) ret = Get_Mod(1ll*ret*x); return ret;}
+
+    vector<ll> Fac,Fac_inv;
+    void Prepare_Factorium(ll n) {Fac.clear();Fac.resize(n+1);Fac[0]=Fac[1]=1; Fac_inv.clear();Fac_inv.resize(n+1);Fac_inv[0]=Fac_inv[1]=1; FORLL(i,2,n) {Fac[i]=Get_Mod(Fac[i-1]*i);Fac_inv[i]=CCLIB::qcpow(Fac[i],MOD-2);}}void Prepare_Combination(ll n){Prepare_Factorium(n);}
+    ll Get_Combination(ll m,ll n) {return Get_Mod(Get_Mod(Fac[m]*Fac_inv[m-n])*Fac_inv[n]);}
+
+    vector<ll> Nums;
+    void Get_Nums(string s){ Nums.clear(); ll n=s.length();ll t=0;int flag=0; FORLL(i,0,n-1) if(s[i]<='9'&&s[i]>='0'){t*=10;t+=s[i]-'0';flag++;}else if(flag){Nums.emplace_back(t);t=0;flag=0;} if(flag){Nums.emplace_back(t);t=0;flag=0;}}
+
+    template<class T>
+    void print_vec(const T &A){for(auto &x:A) cout << x << ' ';cout << endl;}
+    template<class T>
+    void print_float(T value,int digit=10){cout << fixed << setprecision(digit) << value;}
+    
+}
+
+namespace MOLDULE
+{
+    inline ll inv(ll x) {return CCLIB::qcpow(x,MOD-2);}
+    inline ll add(ll x, ll y) {return Get_Mod(x + y);}
+    inline ll addto(ll &x, ll y) {return x = add(x, y);}
+    inline ll sub(ll x, ll y) {return Get_Mod(x - y);}
+    inline ll subto(ll &x, ll y) {return x = sub(x, y);}
+    inline ll mul(ll x, ll y) {return Get_Mod(1ll*x * y);}
+    inline ll multo(ll &x, ll y) {return x = mul(x, y);}
+    inline ll div(ll x, ll y) {return Get_Mod(1ll*x*inv(y));} 
+    inline ll divto(ll &x, ll y) {return x = div(x, y);}
+}
+
+
+#define ONLINE_JUDGE
+#define FAST_IO
+#define MUTIPLE_JUDGE
+//#define CHECK_OUT_TIME
+
+using namespace DEFINITION;
+//using namespace MOLDULE;
+using namespace CCLIB;
+
+/*----------Code Area----------*/
+#define N 200005
+void solve()
+{
+    ll n,m,t,tm;cin >> n >> m;
+    create_vec(a,n);
+    vector<pll> q,tq;
+    ll u,v;
+    FORLL(i,1,m){
+        cin >> u >> v;
+        if(u>v) swap(u,v);
+        if(u==1) a[0]+=3;
+        else q.pb({u,v});
+    }   
+    for(auto x:q){
+        if(a[x.first-1]>a[0]) {a[x.first-1]+=3;continue;}
+        if(a[x.second-1]>a[0]) {a[x.second-1]+=3;continue;}
+        tq.pb(x);
+    }
+    q=tq;tq.clear();
+    for(auto x:q){
+        if(a[x.first-1]<=a[0]-1&&a[x.second-1]<=a[0]-1) {a[x.first-1]++;a[x.second-1]++;continue;}
+        tq.pb(x);
+    }
+    q=tq;tq.clear();
+    for(auto x:q){
+        if(a[x.first-1]<=a[0]-3) {a[x.first-1]+=3;continue;}
+        if(a[x.second-1]<=a[0]-3) {a[x.second-1]+=3;continue;}
+        tq.pb(x);
+    }
+    q=tq;tq.clear();
+    SORT(q);
+    map<ll,ll> mp;
+    while(q.size()){
+        mp.clear();t=tm=0;
+        for(auto x:q) {mp[x.first]++;mp[x.second]++;}
+        FORLL(i,1,n) if(mp[i]>tm) {tm=mp[i];t=i;}
+        for(auto x:q){
+            if(x.first==t||x.second==t){
+                a[t-1]+=3;
+                continue;
+            }
+            tq.pb(x);
+        }q=tq;tq.clear();
+    }
+    ll ans=0;
+    for(auto x:a) if(x>a[0]) ans++;
+    cout << ans+1 << endl;
+}
+/*----------Code Area----------*/
+
+int main(){
+    int clk = clock();
+
+#ifndef ONLINE_JUDGE
+    if(freopen("in.txt", "r", stdin)==NULL) {cout << "Fail opening in.txt!" << endl;return 0;}
+    if(freopen("out.txt", "w", stdout)==NULL) {cout << "Fail opening out.txt!" << endl;return 0;}
+#endif
+
+#ifdef FAST_IO
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+#endif
+
+#ifdef MUTIPLE_JUDGE
+    long T; cin >> T;
+    while(T--) solve();
+#else
+    solve();
+#endif
+
+#ifdef CHECK_OUT_TIME
+    cout << clock() - clk << " ms\n";
+#endif
+    return 0;
+}
